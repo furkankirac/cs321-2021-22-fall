f6b99e8be063fd39c35f5ac8a9433cb2d308ea66
Furkan Kirac <fkirac@gmail.com>
2022-01-03 17:14:02 +0300

    Week 15 Final Solutions

--- week15-final.cpp
+++ week15-final.cpp
@@ -2,7 +2,7 @@
 // Write your COMPILER and its VERSION here
 
 // ---- READ CAREFULLY ----
-// During the exam, you MUST upload your code snapshots at 10:00, and finally 11:30 or before the exam ends.
+// During the exam, you MUST upload your code snapshots at 10:30, and finally 11:30 or before the exam ends.
 // ALWAYS CHECK YOUR UPLOADED CODE BY DOWNLOADING IT BACK FROM THE LMS.
 // IN CASE OF A PROBLEM, EMAIL YOUR SNAPSHOT TO THE INSTRUCTOR AND ALL TEACHING ASSISTANTS.
 
@@ -57,32 +57,32 @@ auto& operator<<(std::ostream& os, const StockRecord& sr)
 
 #include <range/v3/all.hpp>
 
-namespace views = ranges::views; // instead of ranges::views::*, directly use views::*
+    namespace views = ranges::views; // instead of ranges::views::*, directly use views::*
 namespace actions = ranges::actions; // instead of ranges::views::*, directly use views::*
 using namespace ranges; // instead of ranges::*, directly use *
 
-int main(int argc, char* argv[])
+    int main(int argc, char* argv[])
 {
     auto stock_data = vector<StockRecord>{
-        {"2021-11-11", {"Motherboard", 2}},
-        {"2021-12-02", {"SSD", 21}},
-        {"2021-11-05", {"CPU", 12}},
-        {"2021-11-21", {"GPU", 35}},
-        {"2021-11-23", {"Keyboard", 3}},
-        {"2021-11-11", {"Mouse", 20}},
-        {"2021-12-02", {"Monitor", 10}},
-        {"2021-12-11", {"CPU", 5}},
-    };
+                                          {"2021-11-11", {"Motherboard", 2}},
+                                          {"2021-12-02", {"SSD", 21}},
+                                          {"2021-11-05", {"CPU", 12}},
+                                          {"2021-11-21", {"GPU", 35}},
+                                          {"2021-11-23", {"Keyboard", 3}},
+                                          {"2021-11-11", {"Mouse", 20}},
+                                          {"2021-12-02", {"Monitor", 10}},
+                                          {"2021-12-11", {"CPU", 5}},
+                                          };
 
-    auto price_data = map<string, int>{
-        {"Motherboard", 100},
-        {"SSD", 60},
-        {"CPU", 85},
-        {"GPU", 120},
-        {"Keyboard", 5},
-        {"Mouse", 8},
-        {"Monitor", 45},
-    };
+        auto price_data = map<string, int>{
+                                           {"Motherboard", 100},
+                                           {"SSD", 60},
+                                           {"CPU", 85},
+                                           {"GPU", 120},
+                                           {"Keyboard", 5},
+                                           {"Mouse", 8},
+                                           {"Monitor", 45},
+                                           };
 
     // Q1 (15pts) - Write unique dates in ascending order when at least one stock delivery happened
     // Q2 (15pts) - Write all stock data sorted by ascending date
@@ -103,7 +103,54 @@ int main(int argc, char* argv[])
     // true means that two items are already sorted, false means that they need to be swapped
     // 2nd one is a lambda that returns the necessary portion of the whole item on which the sorting will be handled
 
+    auto get_date = [](const StockRecord& sr) { return sr.date; };
 
+    stock_data |= actions::sort(std::less{}, get_date);
+
+    auto unique_dates = stock_data
+                        | views::transform(get_date)
+                        | views::unique;
+
+    cout << "Unique Dates in Ascending Order:" << endl
+         << unique_dates << endl
+         << endl;
+
+    cout << "Stock Data Sorted by Date in Ascending Order:" << endl
+         << (stock_data | views::all) << endl
+         << endl;
+
+    auto get_stock = [](auto rng) {
+        return ranges::accumulate(rng, map<string, Count>{}, [](const auto& acc, const auto& sr) {
+            auto new_acc = acc;
+            auto [name, count] = sr.product_count;
+            new_acc[name] += count;
+            return new_acc;
+        });
+    };
+
+    { // Total stock today
+        auto total_stock = get_stock(stock_data | views::all);
+        cout << "Total Stock:" << endl
+             << (total_stock | views::all) << endl
+             << endl;
+    }
+
+    { // Total stock on 2021-11-11 (including 2021-11-11)
+        const auto dt = "2021-11-11";
+        auto f = [&](const auto& sr) { return sr.date <= dt; };
+        auto stock = get_stock(stock_data | views::filter(f));
+        cout << "Stock on 2021-11-11:" << endl
+             << (stock | views::all) << endl
+             << endl;
+
+        auto to_price = [&](const auto& product_count) {
+            auto [name, count] = product_count;
+            return price_data[name] * count;
+        };
+        cout << "Stock Values on 2021-11-11:" << endl
+             << (stock | views::transform(to_price)) << endl
+             << endl;
+    }
 
     // WRITE YOUR SOLUTIONS HERE - END //
 
